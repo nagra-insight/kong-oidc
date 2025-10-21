20854460881dac1a4738443e022fcf90a1e18d6c
Stefano Zaninetta <stefano.zaninetta@nagra.com>
2025-10-21 17:54:52 +0200

    fix unit tests
    thanks Claude.

--- test/unit/base_case.lua
+++ test/unit/base_case.lua
@@ -1,5 +1,20 @@
 package.path = package.path .. ";test/lib/?.lua;;" -- kong & co
 
+-- Initialize minimal ngx global for Kong 3.9 constants module
+-- Kong's constants.lua expects these to be available at module load time
+if not _G.ngx then
+    _G.ngx = {
+        DEBUG = 7,
+        INFO = 6,
+        NOTICE = 5,
+        WARN = 4,
+        ERR = 3,
+        CRIT = 2,
+        ALERT = 1,
+        EMERG = 0,
+    }
+end
+
 local Object = require "test.unit.classic"
 local BaseCase = Object:extend()
 
@@ -9,6 +24,5 @@ end
 
 function BaseCase:tearDown()
 end
-
 
 return BaseCase
