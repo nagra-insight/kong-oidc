01bf95e7f71c09335ad7499e7c87e4dde28f77a3
Stefano Zaninetta <stefano.zaninetta@nagra.com>
2026-02-10 14:44:31 +0100

    load secret from env var

--- kong/plugins/oidc/session.lua
+++ kong/plugins/oidc/session.lua
@@ -1,10 +1,13 @@
 local M = {}
 
+-- Cache the env var at module load time
+local SESSION_SECRET = os.getenv("KONG_SESSION_SECRET")
+
 function M.configure(config)
-    if config.session_secret then
-        local decoded_session_secret = ngx.decode_base64(config.session_secret)
+    if SESSION_SECRET then
+        local decoded_session_secret = ngx.decode_base64(SESSION_SECRET)
         if not decoded_session_secret then
-            kong.log.err("Invalid plugin configuration, session secret could not be decoded")
+            kong.log.err("Invalid session secret from env var KONG_SESSION_SECRET, could not be decoded")
             return kong.response.error(ngx.HTTP_INTERNAL_SERVER_ERROR)
         end
         ngx.var.secret = decoded_session_secret
